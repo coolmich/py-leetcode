448abe3a58c930f36d2f1899a50015b9f897f952
coolmich <coolmich00@gmail.com>
2016-06-11 15:51:36 -0700

    add bash submit

--- submit.sh
+++ submit.sh
@@ -4,7 +4,7 @@ read -p "Enter the file(s) to stage: " ff
 git add "$ff"
 read -p "Enter problem number or complete commit message: " ans
 if ! [[ $ans =~ ^[0-9]+$ ]];then
-    git commit -m $ans
+    git commit -m "$ans"
 else
     git commit -m "Complete Problem $ans"
 fi
