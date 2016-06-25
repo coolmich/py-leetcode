c6826a69ed015e640eaecda670fccba00cf5437f
coolmich <coolmich00@gmail.com>
2016-06-25 15:50:21 -0700

    Complete Problem 98

---
+++ solu/98|Valid Binary Search.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root: return True
+        li = []
+        self.helper(root, li)
+        for i in range(1, len(li)):
+            if li[i] <= li[i-1]: return False
+        return True
+
+    def helper(self, node, li):
+        if node.left: self.helper(node.left, li)
+        li.append(node.val)
+        if node.right: self.helper(node.right, li)
+        
