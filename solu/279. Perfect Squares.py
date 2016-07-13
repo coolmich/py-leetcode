a67778dbdf43d425dd217f181cd5eb3fe4bce762
coolmich <coolmich00@gmail.com>
2016-07-13 09:07:40 -0700

    279, TLE but this dp should be the right way

---
+++ solu/279. Perfect Squares.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 1: return 0
+        res = [0, 1]
+        for i in range(2, n+1):
+            j, mini = 1, 2**31
+            while j**2 <= i:
+                mini = min(mini, 1 + res[i - j**2])
+                j += 1
+            res.append(mini)
+        return res[-1]
