a1950c9474518b7db014b515bb91566ef56cfb6e
coolmich <coolmich00@gmail.com>
2016-08-08 23:19:44 -0700

    294,361

---
+++ solu/294. Flip Game II.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def canWin(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        for i in range(len(s)-1):
+            if s[i:i+2] == '++' and not self.canWin(s[:i] + '-' + s[i+2:]): return True
+        return False
