fbb383f39f17495c829ad6ccdac74ddcf80050eb
coolmich <coolmich00@gmail.com>
2016-08-31 09:09:35 -0700

    332 using dfs for euler path algorithm

--- solu/332. Reconstruct Itinerary.py
+++ solu/332. Reconstruct Itinerary.py
@@ -1,3 +1,29 @@
+# from collections import defaultdict
+# class Solution(object):
+#     def findItinerary(self, tickets):
+#         """
+#         :type tickets: List[List[str]]
+#         :rtype: List[str]
+#         """
+#         def dfs(dict, src, res, tickets):
+#             if len(res) == tickets: return True
+#             if not len(dict[src]): return False
+#             sortedL = sorted(dict[src])
+#             for dst in sortedL:
+#                 dict[src].remove(dst)
+#                 res.append(dst)
+#                 if dfs(dict, dst, res, tickets):
+#                     return True
+#                 res.pop()
+#                 dict[src].append(dst)
+#             return False
+#
+#         dep, res, ticket_num = defaultdict(list), ['JFK'], len(tickets)+1
+#         for u, v in tickets:
+#             dep[u].append(v)
+#         dfs(dep, 'JFK', res, ticket_num)
+#         return res
+
 from collections import defaultdict
 class Solution(object):
     def findItinerary(self, tickets):
@@ -5,22 +31,14 @@ class Solution(object):
         :type tickets: List[List[str]]
         :rtype: List[str]
         """
-        def dfs(dict, src, res, tickets):
-            if len(res) == tickets: return True
-            if not len(dict[src]): return False
-            sortedL = sorted(dict[src])
-            for dst in sortedL:
-                dict[src].remove(dst)
-                res.append(dst)
-                if dfs(dict, dst, res, tickets):
-                    return True
-                res.pop()
-                dict[src].append(dst)
-            return False
-
-        dep, res, ticket_num = defaultdict(list), ['JFK'], len(tickets)+1
+        ed, res = defaultdict(list), []
         for u, v in tickets:
-            dep[u].append(v)
-        dfs(dep, 'JFK', res, ticket_num)
-        return res
+            ed[u].append(v)
+        for u in ed: ed[u] = sorted(ed[u])[::-1]
+        def dfs(city):
+            while ed[city]: dfs(ed[city].pop())
+            res.append(city)
+        dfs("JFK")
+        return res[::-1]
 
+print Solution().findItinerary([["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]])
